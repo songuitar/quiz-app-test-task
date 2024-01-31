892b481c3488d44301617e21670c2ace12f3c778
alexey.bezborodov <songuitar@yandex.ru>
2024-01-31 11:48:51 +0300

    migration cleanup

--- migrations/Version20240131081032.php
+++ migrations/Version20240131081032.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240131074652 extends AbstractMigration
+final class Version20240131081032 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -28,21 +28,6 @@ final class Version20240131074652 extends AbstractMigration
         $this->addSql('CREATE TABLE question (id INT NOT NULL, question_text VARCHAR(255) NOT NULL, answer_options JSON NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE test_result (id INT NOT NULL, conducted_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN test_result.conducted_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
-        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
-        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
-        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
-            BEGIN
-                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
-                RETURN NEW;
-            END;
-        $$ LANGUAGE plpgsql;');
-        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
-        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
         $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A25853A2189 FOREIGN KEY (test_result_id) REFERENCES test_result (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
@@ -57,6 +42,5 @@ final class Version20240131074652 extends AbstractMigration
         $this->addSql('DROP TABLE answer');
         $this->addSql('DROP TABLE question');
         $this->addSql('DROP TABLE test_result');
-        $this->addSql('DROP TABLE messenger_messages');
     }
 }
